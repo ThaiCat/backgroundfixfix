473e9d7b9c9b44e61cf5a550e7b062ee99635414
jhon snow <nameless.ei47@gmail.com>
2025-06-04 13:32:54 +0600

    ---

--- worker.js
+++ worker.js
@@ -1,13 +1,17 @@
 // worker.js
-function getRandomInt(aMin, aMax) {
-  return Math.floor(Math.random() * (aMax - aMin)) + aMin;
+function getRandomInt(aMin, aMax) 
+{
+    return Math.floor(Math.random() * (aMax - aMin)) + aMin;
 }
 
-console.log('Worker запущен!'); // Не сработает, Worker не имеет доступа к консоли страницы
-self.addEventListener('message', () => {
-  function ping() {
-    self.postMessage({ type: 'ping' });
-    setTimeout(ping, getRandomInt(12000, 18000));
-  }
-  ping();
+console.log('Worker running!'); // Не сработает, Worker не имеет доступа к консоли страницы
+self.addEventListener('message', () => 
+{
+    function ping() 
+    {
+        self.postMessage({ type: 'ping' });
+        setTimeout(ping, getRandomInt(12000, 18000));
+    }
+
+    ping();
 });
